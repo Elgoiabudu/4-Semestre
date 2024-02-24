697146ec7d1f6cd9129adcabdf26ce284da0c4c5
Elgoiabudu <fernandobondeleonelli@gmail.com>
2024-02-24 13:03:31 -0300

    Aula Finalizada

--- app/src/main/java/lucas/mend0n/app_intents/Tela2Activity.kt
+++ app/src/main/java/lucas/mend0n/app_intents/Tela2Activity.kt
@@ -1,11 +1,23 @@
 package lucas.mend0n.app_intents
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.graphics.Color
+import android.view.View
 
 class Tela2Activity : AppCompatActivity() {
+
+    lateinit var textView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+
+        textView = findViewById(R.id.textView)
+        textView.setTextColor( Color.rgb(255,50,50) )
+        textView.setTextSize(60.6F)
+
     }
 }
