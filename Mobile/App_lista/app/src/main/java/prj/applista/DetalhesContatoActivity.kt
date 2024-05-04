c659363275f6be39d92eb0d182bba80ea4aa27d0
Elgoiabudu <fernandobondeleonelli@gmail.com>
2024-05-04 13:01:18 -0300

    Crud Completo com Inserção e salvamento da foto do contato

--- Mobile/App_lista/app/src/main/java/prj/applista/DetalhesContatoActivity.kt
+++ Mobile/App_lista/app/src/main/java/prj/applista/DetalhesContatoActivity.kt
@@ -1,17 +1,21 @@
 package prj.applista
 
+import android.net.Uri
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
 import prj.applista.models.Contato
 
 class DetalhesContatoActivity : AppCompatActivity() {
 
-    var contato:Contato?=null
-    var txtNome:EditText?=null
-    var txtEmail:EditText?=null
-    var txtTelefone:EditText?=null
-    var txtEdereco:EditText?=null
+    var contato: Contato? = null
+    var txtNome: EditText? = null
+    var txtEmail: EditText? = null
+    var txtTelefone: EditText? = null
+    var txtEdereco: EditText? = null
+    var imgFoto: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +25,12 @@ class DetalhesContatoActivity : AppCompatActivity() {
         txtEmail = findViewById(R.id.txtEmail)
         txtTelefone = findViewById(R.id.txtTelefone)
         txtEdereco = findViewById(R.id.txtEndereco)
+        imgFoto = findViewById(R.id.imgFoto)
 
 
         if (this.intent.hasExtra("contato"))
             contato = this.intent.getSerializableExtra("contato") as Contato?
-            preencherContato()
+        preencherContato()
     }
 
     private fun preencherContato() {
@@ -33,6 +38,10 @@ class DetalhesContatoActivity : AppCompatActivity() {
         txtEmail?.setText(contato?.email)
         txtTelefone?.setText(contato?.telefone)
         txtEdereco?.setText((contato?.endereco))
+
+        if (contato?.foto != "#") {
+            imgFoto?.setImageURI(Uri.parse(contato?.foto))
+        }
 
     }
 }
--- Mobile/App_lista/app/src/main/java/prj/applista/FormContatoActivity.kt
+++ Mobile/App_lista/app/src/main/java/prj/applista/FormContatoActivity.kt
@@ -1,11 +1,17 @@
 package prj.applista
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import prj.applista.api.ApiClient
 import prj.applista.interfaces.ContatoService
@@ -13,6 +19,11 @@ import prj.applista.models.Contato
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FormContatoActivity : AppCompatActivity() {
     lateinit var txtFormNome: EditText
@@ -20,7 +31,12 @@ class FormContatoActivity : AppCompatActivity() {
     lateinit var txtFormEmail: EditText
     lateinit var txtFormEndereco: EditText
     lateinit var btFormSalvar: Button
-    var contato:Contato? = null
+    lateinit var imgFotoContato: ImageView
+
+    var contato: Contato? = null
+    val TIRAR_FOTO = 1
+
+    var caminhoFoto: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +48,12 @@ class FormContatoActivity : AppCompatActivity() {
         txtFormTelefone = findViewById(R.id.txtFormTelefone)
         btFormSalvar = findViewById(R.id.btFormSalvar)
 
+        imgFotoContato = findViewById(R.id.imgFotoContato)
+        imgFotoContato.setOnClickListener { tirarFoto() }
+
         btFormSalvar.setOnClickListener { salvarContato() }
 
-        if (intent.hasExtra("contato")){
+        if (intent.hasExtra("contato")) {
 
             contato = intent.getSerializableExtra("contato") as Contato
             exibirDadosContato()
@@ -43,38 +62,90 @@ class FormContatoActivity : AppCompatActivity() {
 
     }
 
+    private fun tirarFoto() {
+
+        var intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, TIRAR_FOTO)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == TIRAR_FOTO) {
+            if (resultCode == RESULT_OK) {
+                val imageBitmap: Bitmap = data?.extras?.get("data") as Bitmap
+                imgFotoContato.setImageBitmap(imageBitmap)
+                caminhoFoto = salvarArquivo(imageBitmap)
+            }
+        }
+    }
+
+    private fun salvarArquivo(imageBitmap: Bitmap): String? {
+
+        var caminho: String? = null
+        var imageFolder = File(filesDir, "images")
+        imageFolder.mkdirs()
+        var timeStamp = SimpleDateFormat("ddMMyyyyhhmmss", Locale.getDefault()).format(Date())
+        var imageFile = File(imageFolder, timeStamp + ".jpg")
+
+        try {
+
+            val fileOut = FileOutputStream(imageFile)
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOut)
+            caminho = imageFile.absolutePath
+
+        } catch (e: Exception) {
+
+            Log.e("Erro", "salvarArquivo: " + e.message)
+
+        }
+
+        Log.i("caminho arquivo", "salvarArquivo: " + caminho)
+
+        return caminho
+    }
+
+
     private fun exibirDadosContato() {
         txtFormNome.setText(contato!!.nome)
         txtFormTelefone.setText(contato!!.telefone)
         txtFormEmail.setText(contato!!.email)
         txtFormEndereco.setText(contato!!.endereco)
+
+        if (contato!!.foto != "#") {
+            imgFotoContato.setImageURI(Uri.parse(contato!!.foto))
+        }
     }
 
 
-
-
     private fun salvarContato() {
-        if(contato== null){
+        if (contato == null) {
             cadastrarContato()
-        }else{
+        } else {
             atualizarContato()
         }
     }
 
-    private fun atualizarContato(){
+    private fun atualizarContato() {
         var call = ApiClient.client.create(ContatoService::class.java)
         preencherDados()
 
-        call.updateContato(contato!!._id, contato!!).enqueue(object : Callback<Contato>{
+        call.updateContato(contato!!._id, contato!!).enqueue(object : Callback<Contato> {
             override fun onResponse(call: Call<Contato>, response: Response<Contato>) {
-                if (response.isSuccessful){
-                    Toast.makeText(this@FormContatoActivity, "Contato atualizado", Toast.LENGTH_LONG).show()
+                if (response.isSuccessful) {
+                    Toast.makeText(
+                        this@FormContatoActivity,
+                        "Contato atualizado",
+                        Toast.LENGTH_LONG
+                    ).show()
                     this@FormContatoActivity.finish()
                 }
             }
 
             override fun onFailure(call: Call<Contato>, t: Throwable) {
-                Toast.makeText(this@FormContatoActivity, "Erro de atualização", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@FormContatoActivity, "Erro de atualização", Toast.LENGTH_LONG)
+                    .show()
             }
 
         })
@@ -86,6 +157,10 @@ class FormContatoActivity : AppCompatActivity() {
         contato!!.endereco = txtFormEndereco.text.toString()
         contato!!.email = txtFormEmail.text.toString()
         contato!!.telefone = txtFormTelefone.text.toString()
+
+        if (caminhoFoto != null) {
+            contato!!.foto = caminhoFoto!!.toString()
+        }
     }
 
     private fun cadastrarContato() {
@@ -111,13 +186,21 @@ class FormContatoActivity : AppCompatActivity() {
     }
 
     private fun lerDadosContato(): Contato {
+
+        var foto = "#"
+
+        if (caminhoFoto != null) {
+            foto = caminhoFoto as String
+        }
+
+
         var _contato = Contato(
             _id = "",
             nome = txtFormNome.text.toString(),
             email = txtFormEmail.text.toString(),
             telefone = txtFormTelefone.text.toString(),
             endereco = txtFormEndereco.text.toString(),
-            foto = "#"
+            foto = foto
         )
 
         return _contato
