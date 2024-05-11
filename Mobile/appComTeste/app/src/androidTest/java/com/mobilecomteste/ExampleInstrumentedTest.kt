2e8a0999fb6af7dbd7122efc55a6a450bfd4647a
Elgoiabudu <fernandobondeleonelli@gmail.com>
2024-05-11 13:01:30 -0300

    Primeiro Teste Implementado com Sucesso

--- Mobile/appComTeste/app/src/androidTest/java/com/mobilecomteste/ExampleInstrumentedTest.kt
+++ Mobile/appComTeste/app/src/androidTest/java/com/mobilecomteste/ExampleInstrumentedTest.kt
@@ -1,5 +1,12 @@
 package com.mobilecomteste
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +22,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class IMCTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
-    fun useAppContext() {
+    fun TesteCalculo() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mobilecomteste", appContext.packageName)
+        onView(withId(R.id.txtPeso)).perform(typeText("100"))
+        onView(withId(R.id.txtAltura)).perform(typeText("1.85"))
+        onView(withId(R.id.btCalcular)).perform(click())
+        onView(withId(R.id.txtIMC)).check(matches(isDisplayed()))
+
     }
 }
--- Mobile/appComTeste/app/src/main/java/com/mobilecomteste/MainActivity.kt
+++ Mobile/appComTeste/app/src/main/java/com/mobilecomteste/MainActivity.kt
@@ -2,10 +2,35 @@ package com.mobilecomteste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtPeso:EditText
+    lateinit var txtAltura:EditText
+    lateinit var txtIMC:TextView
+    lateinit var btCalcular:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtPeso = findViewById(R.id.txtPeso)
+        txtAltura = findViewById(R.id.txtAltura)
+        txtIMC = findViewById(R.id.txtIMC)
+        btCalcular = findViewById(R.id.btCalcular)
+
+        btCalcular.setOnClickListener{
+
+            val peso:Double = txtPeso.text.toString().toDouble()
+            val altura:Double = txtAltura.text.toString().toDouble()
+
+            var imc:Double = peso / (altura * altura)
+
+            txtIMC.text = imc.toString()
+        }
     }
 }
