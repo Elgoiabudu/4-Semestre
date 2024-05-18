fb0f37426b9b1b328e812c21dd67f48cdf912845
Elgoiabudu <fernandobondeleonelli@gmail.com>
2024-05-18 12:14:51 -0300

    Testes implentados com sucesso

--- Mobile/appComTeste/app/src/androidTest/java/com/mobilecomteste/ExampleInstrumentedTest.kt
+++ Mobile/appComTeste/app/src/androidTest/java/com/mobilecomteste/ExampleInstrumentedTest.kt
@@ -1,19 +1,16 @@
 package com.mobilecomteste
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 import org.junit.Rule
 
 /**
@@ -32,7 +29,7 @@ class IMCTest {
     fun TesteCalculo() {
         // Context of the app under test.
         onView(withId(R.id.txtPeso)).perform(typeText("100"))
-        onView(withId(R.id.txtAltura)).perform(typeText("1.85"))
+        onView(withId(R.id.txtAltura)).perform(typeText("2"))
         onView(withId(R.id.btCalcular)).perform(click())
         onView(withId(R.id.txtIMC)).check(matches(isDisplayed()))
 
